8e2698a69563a92982a68ecc18e7fd4eb9e8df77
Ogadi89 <ogadipatrick89@gmail.com>
2022-06-30 06:34:20 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ * main - script that compiles a C file and creates an executable
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
