98611b98dbad87268f9e400d1a8dac765d7c2de1
Ogadi89 <ogadipatrick89@gmail.com>
2022-07-05 16:09:04 -0700

    function that prints all natural numbers from n to 98, followed by a new line

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,12 +11,12 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		while (n <=98)
+		while (n <= 98)
 		{
 			if (n == 98)
-				printf("%d\n" , n);
+				printf("%d\n", n);
 			else
-				printf("%d, " , n);
+				printf("%d, ", n);
 			n = n + 1;
 		}
 	}
@@ -25,9 +25,9 @@ void print_to_98(int n)
 		while (n >= 98)
 		{
 			if (n == 98)
-				printf("%d\n" , n);
+				printf("%d\n", n);
 			else
-				printf("%d, " , n);
+				printf("%d, ", n);
 			n = n - 1;
 		}
 	}
