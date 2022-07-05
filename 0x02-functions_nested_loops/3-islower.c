575fea0cc6eaa72c666468db45cecbd8c5de42b0
Ogadi89 <ogadipatrick89@gmail.com>
2022-07-05 08:42:14 -0700

    Hot fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,7 +4,7 @@
 * _islower -> checks if the character is lowercase
 * Return: returns 1 and 0 depending on condition
 */
-int _islower(int c);
+int _islower(int c)
 {
 	return (c >= 'a' && c <= 'z');
 }
