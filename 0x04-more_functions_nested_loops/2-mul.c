14f7a83bf19f4d69103bf669481a71b84d820b24
Ogadi89 <ogadipatrick89@gmail.com>
2022-07-07 05:46:07 -0700

    multplication of two numbers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * mul -> multiplying 2 numbers
+ * mul -> multiplying two numbers
  * @a: integer parameter1
- * @b: integer paramenter2
+ * @b: interger parameter2
  * Return: success and integer
  */
 
 int mul(int a, int b)
 {
-    
-    return (a * b)
+	return (a * b);
 }
