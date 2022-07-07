b61569220910c965523103467b024b8f6d3bd5c3
Ogadi89 <ogadipatrick89@gmail.com>
2022-07-07 08:16:26 -0700

    prints most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,9 +7,9 @@ void print_most_numbers(void)
 {
 	int x;
 
-	for (x = 0; x < 10; x++)
+	for (x = 0; x< 10; x++)
 	{
-		if (x ! = 2 && x ! = 4)
+		if (x !=2 && x !=4)
 			_putchar(x + '0');
 	}
 	_putchar('\n');
