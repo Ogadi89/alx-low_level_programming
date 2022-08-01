f15a3c8e22e8297ff7192a8ac3f88322e1ce4dcd
Ogadi89 <ogadipatrick89@gmail.com>
2022-08-01 05:00:40 -0700

    Hot fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
 
+/*
+ * File: dog.h
+ * Desc: Header file that defines a new type struct dog.
+ */
+
 /**
  * struct dog - A new type describing a dog.
  * @name: The name of the dog.
