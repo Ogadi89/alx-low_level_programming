00cec597565f10b4e183340107bcd49178b7049f
Ogadi89 <ogadipatrick89@gmail.com>
2022-10-03 20:14:31 -0700

    pushing required files

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,19 @@
+/**
+ * _strlen - Returns the length of a string
+ * @s: string name
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
--- 0x18-dynamic_libraries/README.md
+++ 0x18-dynamic_libraries/README.md
@@ -1,2 +1,1 @@
-0x18-dynamic_libraries
-This project, shows how to create dynamic libraries from modules and use them
+ Thid is a Documentation for this Folder
